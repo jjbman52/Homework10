a91af31188af3fb8b421b68ab738a618076e1887
jjbman52 <jjbman52@gmail.com>
2019-10-29 21:32:21 -0500

    Initial check-in of module Homework10

---
+++ Homework10/MainPage.xaml.cs
@@ -0,0 +1,65 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Collections.ObjectModel;
+using System.ComponentModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Xamarin.Forms;
+
+namespace Homework10
+{
+    // Learn more about making custom code visible in the Xamarin.Forms previewer
+    // by visiting https://aka.ms/xamarinforms-previewer
+    [DesignTimeVisible(false)]
+    public partial class MainPage : ContentPage
+    {
+        public static ObservableCollection<string> items { get; set; }
+
+        public MainPage()
+        {
+            items = new ObservableCollection<string>() { "Speaker", "Pen", "Lamp", "Monitor", "Bag", "Book", "Cap", "Tote", "Floss", "Phone" };
+            InitializeComponent();
+        }
+
+        void OnSelection(object sender, SelectedItemChangedEventArgs e)
+        {
+            if (e.SelectedItem == null)
+            {
+                return;
+            }
+            DisplayAlert("Item Selected", e.SelectedItem.ToString(), "Ok");
+        }
+
+        void OnRefresh(object sender, EventArgs e)
+        {
+            var list = (ListView)sender;
+            //put your refreshing logic here
+            var itemList = items.Reverse().ToList();
+            items.Clear();
+            foreach (var s in itemList)
+            {
+                items.Add(s);
+            }
+            //make sure to end the refresh state
+            list.IsRefreshing = false;
+        }
+
+        void OnTap(object sender, ItemTappedEventArgs e)
+        {
+            DisplayAlert("Item Tapped", e.Item.ToString(), "Ok");
+        }
+
+        void OnMore(object sender, EventArgs e)
+        {
+            var item = (MenuItem)sender;
+            DisplayAlert("More Context Action", item.CommandParameter + " more context action", "OK");
+        }
+
+        void OnDelete(object sender, EventArgs e)
+        {
+            var item = (MenuItem)sender;
+            items.Remove(item.CommandParameter.ToString());
+        }
+    }
+}
